1cf03693886ae33cefe0a36c0df738470bf9072a
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2020-03-08 19:46:15 -0400

    week4

---
+++ Weel4/pausePractice/index.js
@@ -0,0 +1,38 @@
+ document.getElementById("box").addEventListener("dblclick", function(){
+    box.style.backgroundColor = "green"
+ })
+
+ document.getElementById("box").addEventListener("mousedown", function(){
+    box.style.backgroundColor = "red"
+ })
+
+ document.getElementById("box").addEventListener("mouseover", function(){
+    box.style.backgroundColor = "blue"
+ })
+
+ document.getElementById("box").addEventListener("mouseup", function(){
+    box.style.backgroundColor = "yellow"
+ })
+
+ document.getElementById("box").addEventListener("wheel", function(){
+    box.style.backgroundColor = "orange"
+ })
+
+ document.getElementById("box").addEventListener("keypress", function(){
+    box.style.backgroundColor = "pink"
+ })
+// Can not figure out how to get this to work
+ //function myFunction(event) {
+   // var x = event.keyCode;
+    //if (x == 82) { 
+     //box.style.backgroundColor = "red"
+    //}else if(x === 79){
+     // box.style.backgroundColor = "orange"
+    //} else if(x == 66){
+     // box.style.backgroundColor = "blue"
+    //}else if(x == 89){
+     // box.style.backgroundColor = "yellow"
+    //}else (x == 71){
+      //box.style.backgroundColor = "green"
+    //}
+   //}
