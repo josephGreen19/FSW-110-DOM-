839a2d3b919b4057ec5e469f70043e8f9c49d2d1
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2020-05-27 17:33:53 -0400

    mario

---
+++ Week6/Project3/app.js
@@ -0,0 +1,38 @@
+function pest1()
+{
+    let field1=document.getElementById("num1").value; 
+    let field2=5 
+
+    let result=parseInt(field1)*parseInt(field2);
+
+    if((result))
+    {
+        document.getElementById("answer").innerHTML="The price is "+result + " coins!";
+    }
+}
+function pest2()
+{
+    let field1=document.getElementById("num3").value; 
+    let field2= 7
+
+    let result=parseInt(field1)*parseInt(field2);
+
+    if((result))
+    {
+        document.getElementById("answer2").innerHTML="The price is "+result + " coins!";
+    }
+}
+
+function pest3()
+{
+    let field1=document.getElementById("num5").value; 
+    let field2= 11 
+
+    let result=parseInt(field1)*parseInt(field2);
+
+    if((result))
+    {
+        document.getElementById("answer3").innerHTML="The price is "+result + " coins!";
+    }
+}
+
